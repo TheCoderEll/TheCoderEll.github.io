d241b65b917af73d5b8146fa80b46c9e78040f04
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-01-09 16:09:32 +0000

    Initial commit

---
+++ najbdskjbbjsdg.js
@@ -0,0 +1,15 @@
+n1 = document.getElementById("n1");
+n2 = document.getElementById("n2");
+ans = document.getElementById("result");
+function multi(){
+	ans.text = n1 * n2;
+}
+function divide(){
+	ans.text = n1 / n2;
+}
+function add(){
+	ans.text = n1 + n2;
+}
+function subtrack(){
+	ans.text = n1 - n2;
+}
