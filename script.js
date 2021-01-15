5501d6ee3efe9eff544dfc230eeef060feb701aa
TheCoderEll <75807962+TheCoderEll@users.noreply.github.com>
2021-01-15 14:59:15 +0000

    Initial commit

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function ucp(input) {
 }
 
  function popup(mylink, windowname) { if (! window.focus)return true; var href; if (typeof(mylink) == 'string') href=mylink; else href=mylink.href; window.open(href, windowname, 'width=400,height=200,scrollbars=yes'); return false; } 
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
